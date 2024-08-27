be55bcf34bc9402d0918995a6735e5ec3d4402f6
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2024-08-27 22:14:51 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import { useState } from 'react';
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, Image, Input, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, useDisclosure} from "@nextui-org/react";
 import { Link } from 'react-router-dom';
